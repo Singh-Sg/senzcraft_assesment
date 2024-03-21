4cf5d750dc75b1421ae1e4e425b667db15667e8a
developer <deependrasg@gmail.com>
2024-03-21 20:18:29 +0530

    added flask sql schema

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
-import unittest
-import json
-from app import app
 
+import unittest
+from app import app
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
+        """
+        Set up the test client and configure app for testing.
+        """
         self.app = app.test_client()
         app.testing = True
 
     def test_uploadfile_endpoint(self):
+        """
+        Test the uploadfile endpoint.
+
+        It uploads a PDF file and checks if the response is successful.
+        """
         pdf_file = open("interest.pdf", "rb")
         response = self.app.post("/uploadfile", data={"file": pdf_file})
         self.assertEqual(response.status_code, 201)
@@ -16,6 +23,11 @@ class TestAPI(unittest.TestCase):
         self.assertIn(b"true", response.data)
 
     def test_get_contacts(self):
+        """
+        Test the get_contacts endpoint.
+
+        It checks if the response contains necessary fields for each contact.
+        """
         response = self.app.get("/contact")
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
@@ -31,6 +43,11 @@ class TestAPI(unittest.TestCase):
             self.assertIsInstance(contact["interests"], list)
 
     def test_get_contact_existing_by_id(self):
+        """
+        Test the get_contact endpoint with an existing contact ID.
+
+        It checks if the response contains necessary fields for the contact.
+        """
         response = self.app.get("/contact/2")
         data = response.get_json()
         self.assertEqual(response.status_code, 200)
@@ -43,14 +60,17 @@ class TestAPI(unittest.TestCase):
         self.assertIn("interest", data)
 
     def test_get_contact_non_existing_by_id(self):
+        """
+        Test the get_contact endpoint with a non-existing contact ID.
+
+        It checks if the response contains an error message.
+        """
         response = self.app.get("/contact/999")
         data = response.get_json()
         self.assertEqual(response.status_code, 404)
         self.assertIn("error", data)
         self.assertEqual(data["error"], "Contact not found")
-
         print(data)
-
 
 if __name__ == "__main__":
     unittest.main()
